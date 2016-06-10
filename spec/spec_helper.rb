ec233dec7cc06da3f6f64f00948337566d389957
cignoir <cignoir@gmail.com>
2016-06-10 11:38:27 +0900

    Use coverall

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'coveralls'
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 Dir[File.join(File.dirname(__FILE__), "..", "lib", "**/*.rb")].each{|f| require f }
 
